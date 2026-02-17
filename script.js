70dd4c5e9ab6e7047fd9e9279b48a31ae9fb833a
dianedoan <Dianedd419@hotmail.com>
2026-02-17 13:06:02 -0700

    Add operate function that calls corresponding math operator function on two numbers

--- script.js
+++ script.js
@@ -23,3 +23,18 @@ function divide(num1, num2) {
 let number1 = "";
 let operator = "";
 let number2 = "";
+
+// takes an operator and two numbers and calls one of the basic math operation functions on the two numbers
+function operate(operator, num1, num2) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator ==="/") {
+        return divide(num1, num2);
+    } else {
+        return "invalid operator";
+    }
+}

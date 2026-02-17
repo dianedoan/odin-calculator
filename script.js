1f7c5d40bc432941935515d9e92ca2ab0695fc40
dianedoan <Dianedd419@hotmail.com>
2026-02-17 15:16:48 -0700

    Add clear button functionality

--- script.js
+++ script.js
@@ -19,11 +19,6 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-// variables for html display
-let number1 = "";
-let operator = "";
-let number2 = "";
-
 // takes an operator and two numbers and calls one of the basic math operation functions on the two numbers
 function operate(operator, num1, num2) {
     if (operator === "+") {
@@ -39,19 +34,44 @@ function operate(operator, num1, num2) {
     }
 }
 
+// variables for html display
+let number1 = "";
+let operator = "";
+let number2 = "";
+
 // get elements
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll("button");
 
-// updates the display with number1 when the calculator's digit buttons are clicked
+// updates the display with number1/number2 when the calculator's digit buttons are clicked
 function updateDigit(digit) {
     // before operator button clicked
     if (operator === "") {
         number1 += digit;
+
+        // display number
         display.textContent = number1;
     } else { // after operator button clicked
         number2 += digit;
+
+        // display number
         display.textContent = number2;
+    }
+}
+
+// calls operate function and displays result
+function handleExpression() {
+    // ensure two numbers and an operator have been clicked
+    if (number1 && operator && number2) {
+        // call operate function
+        const result = operate(operator, Number(number1), Number(number2));
+
+        // display calculated result
+        display.textContent = result;
+
+        // set variables
+        number1 = result;
+        number2 = "";
     }
 }
 
@@ -60,6 +80,18 @@ buttons.forEach((button) => {
     // add a 'click' event listener for each button
     button.addEventListener("click", () => {
         const value = button.textContent;
+
+        // clear button
+        if (value === "C") {
+            // display reset value
+            display.textContent = 0;
+
+            // reset variables
+            number1 = "";
+            operator = "";
+            number2 = "";
+            return;
+        }
         
         // update display with digit
         updateDigit(value);
